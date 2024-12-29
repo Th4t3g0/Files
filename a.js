c1e7d2e1a7e3fd6b86a11ed90627533daf710aad
Thatego Fritz <113315881+Th4t3g0@users.noreply.github.com>
2024-12-30 00:04:19 +0200

    second commit a.js

--- a.js
+++ a.js
@@ -4,6 +4,10 @@ let j=1;
 let positionList = [];
 let bigFile = null;
 let smallFile = null;
+let c =7;
+let name ="";
+
+
 
 class Node{
    constructor(data) {
@@ -40,6 +44,20 @@ class LinkedList {
            return;
         }
    }
+    
+    getData(position,head){
+        
+        if(position<1){
+            return
+        }
+        if (position==1) {
+            return head;
+        }
+        
+        this.getData(position-1,head.next)
+        
+    }
+    
     displayData(head){
         
         if(head == null){
@@ -69,21 +87,23 @@ class LinkedList {
 function Match(head1, head2) {
     let current1 = head1;
     let current2 = head2;
-    let j = 2;
+    let jj = 2;
     //console.log("matching");
     while (current1 != null) {
         current2 = head2;
-          
+          //jj=2;
         while (current2 != null) {
+            
             if (current1.partNo == current2.partNo) {
-                positionList.push(j);
-                console.log(current1.partNo);
-                console.log(j);
+                positionList.push(current1.position);
+                // console.log(current1.partNo);
+                // console.log(current1.position);
                 break;
             }
+            //jj++;
             current2 = current2.next;
         }
-        j++;  // j is incremented after the inner loop is done processing the current1 element
+          // j is incremented after the inner loop is done processing the current1 element
         current1 = current1.next;
     }
 }
@@ -122,12 +142,12 @@ function ReadCSVFile(file) {
                     continue;
                 }else{
                     if (row.trim() === "") { continue; }
-                    myList1.append(row);
+                    myList2.append(row);
                     j++;
                 } 
             }
             j=1;
-            //myList1.displayPartNumber(myList1.head);
+            //myList2.displayPartNumber(myList2.head);
         };        
         reader.readAsText(file);
     } else {
@@ -147,12 +167,12 @@ function ReadWriteCSVFile(file) {
                     continue;
                 }else{
                     if (row.trim() === "") { continue; }
-                    myList2.append(row,j);
+                    myList1.append(row,j);
                     j++;
                 } 
             }
-            j=1;
-            //myList2.displayPosition(myList2.head);
+            //j=1;
+            //myList1.displayPosition(myList1.head);
             
         };  
      
@@ -164,65 +184,66 @@ function ReadWriteCSVFile(file) {
 }
 
 ///
-// function downloadCSV(data) {
-//     const blob = new Blob([data], { type: 'text/csv' });
-//     const link = document.createElement('a');
-//     link.href = URL.createObjectURL(blob);
-//     link.download = 'modified_data.csv';
-//     link.click();
-// }
+function downloadCSV(data) {
+    const blob = new Blob([data], { type: 'text/csv' });
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = 'modified_data.csv';
+    link.click();
+}
 
-//////
+function Replace() {
+for (let ii = 0; ii < bound; ii++) {
 
-// function processCSV(file) {
-//     const reader = new FileReader();
-//     reader.onload = function(e) {
-//         const csvContent = e.target.result;
-//         let rows = csvContent.split('\n');
-        
-//         ///update the CSV adding what hwat
+    let s = myList1.getData(positionList[ii],myList1.head).split(",");
+    s[c]=name;
 
-//         
-//         const updatedCSV = rows.join('\n');
 
-//         
-//         downloadCSV(updatedCSV);
-//     };
-//     reader.readAsText(file);
-// }
+}
+}
+
+
+
+function processCSV(file) {
+    
+    let l=positionList.length;
+    
+for (let ii = 0; ii < l; ii++) {
+let s = myList1.getData(positionList[ii],myList1.head).split(",");
+s[c]=name;
+
+}
+    const updatedCSV = myList1.getData(positionList[ii],myList1.head).join('\n');
+    downloadCSV(updatedCSV);
+
+    
+}
+
 
 
 
 
 document.getElementById('bigFileInput').addEventListener('change', function(event) {
-    // const file = event.target.files[0];
-    // ReadCSVFile(file);
     file1 = event.target.files[0];
-   
-   
 });
 
 document.getElementById('smallFileInput').addEventListener('change', function(event) {
-    // const file = event.target.files[0];
-    // ReadCSVFile(file);
    file2 = event.target.files[0];
    
 });
 document.getElementById('startProcessingButton').addEventListener('click', function(event) {
-
+    name = document.getElementById("partJobNumber").value;
     ReadWriteCSVFile(file1);
     ReadCSVFile(file2);
-    ///
+    
     setTimeout(() => {
         Match(myList1.head, myList2.head);
+        processCSV()
     }, 3000);
-
-
-    ///
-   ///Match(myList1.head,myList2.head)
-   
-   
+   console.log(name);
+    
 });
 
 const myList1 = new LinkedList();
 const myList2 = new LinkedList();
+
